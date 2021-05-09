340eef06ed900989ae6be6258da068d765eb41dc
phimth <tphiman@gmail.com>
2021-05-09 15:09:27 +0200

    diplay all elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react'
+import mails from './providers.json'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App (){
+  const [name, setName] = useState("")
+
+  const [display, setDisplay] = useState(mails)
+
+  const change = (e) =>{
+    console.log(e)
+    setName(e)
+  }
+  useEffect(()=>{
+    if(name.includes("@"))console.log('@')
+  },[name])
+
+  return(
+    <div className="flexbox-container">
+    <input 
+      type="text"
+      value={name}
+      onChange={e=>change(e.target.value)}
+      >
+    </input>
+    {display.map((mail)=>{return(
+      <p key={mail} style={{"padding":5}}>
+        {mail}
+      </p>
+      )
+      })}
     </div>
-  );
+  )
 }
+
 
 export default App;
